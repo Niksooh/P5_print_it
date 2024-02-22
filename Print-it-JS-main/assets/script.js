012a62bc2e480d22d77105168f2a3ad51a06a5ba
Niksooh <dymitri.thomas@gmail.com>
2024-02-22 09:55:40 +0100

    Ajout de variable + ajout commentaire

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,21 +1,45 @@
+// Tableau de diapositives
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-myBanner = document.getElementById("banner")
+// Variable
+let i = 0
+let myBanner = document.getElementById("banner")
+let mooveOnRight = document.getElementById("right")
+let mooveOnLeft = document.getElementById("left")
+let dotsContainer = document.querySelector(".dots");
+
+// Mouvement vers la droite aprés le click sur la flêche
+mooveOnRight.addEventListener("click", () => {
+	i = i + 1
+	let img = slides[i]
+	document.querySelector(".banner-img").src = "./assets/images/slideshow/" + img.image
+	document.querySelector("#banner p").innerHTML = img.tagLine
+	dynamicBullets(i);
+})
+
+// Mouvement vers la droite aprés le click sur la flêche
+mooveOnLeft.addEventListener("click", () => {
+	i = i - 1
+	let img = slides[i]
+	document.querySelector(".banner-img").src = "./assets/images/slideshow/" + img.image
+	document.querySelector("#banner p").innerHTML = img.tagLine
+	dynamicBullets(i);
+})
 
